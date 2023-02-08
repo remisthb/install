971c2f838243429fbe92b61522d3a0fed55abe26
2nd1rst <2nd1rst@gmail.com>
2023-02-08 00:58:40 -0500

    5

--- archinstall.sh
+++ archinstall.sh
@@ -6,11 +6,11 @@ bootdrive="${drive}1"
 cryptdrive="${drive}2"
 swap="2G"
 micro="amd-ucode" 
+sgdisk -Zo "$drive"
 cryptsetup luksFormat "$drive"
 cryptsetup open --type plain -d /dev/urandom "$drive" crypt
 dd if=/dev/zero of=/dev/mapper/crypt status=progress 
 cryptsetup close crypt
-sgdisk -Zo "$cryptdrive"
 sgdisk -n 1:2048:+512M -t 1:ef00 -c 1:boot "$drive"
 sgdisk -n 2:0:0 -t 2:8300 -c 2:root "$drive"
 cryptsetup luksFormat "$cryptdrive"
@@ -29,4 +29,8 @@ mount --mkdir "$bootdrive" /mnt/boot
 reflector --country US --age 24 --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 pacstrap -K /mnt base base-devel git linux linux-firmware iwd vim lvm2 "$micro" 
 genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+sed -i "/^HOOKS=/c\HOOKS=(base udev autodetect modconf kms keyboard keymap consolefont block lvm2 encrypt filesystems fsck)/" /etc/mkinitcpio.conf
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
 
