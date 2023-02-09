d89da4f008a3927a5d59175fc9b4f50900aee958
Hunter <2nd1rst@gmail.com>
2023-02-09 10:45:04 -0500

    2

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo pacman -Syu
-sudo pacman -S qutebrowser xclip ranger picom pcmanfm p7zip openssh neofetch mpv man-pages man-db lxappearance htop feh bash-completion reflector
+sudo pacman -S firefox xclip ranger picom pcmanfm p7zip openssh neofetch mpv man-pages man-db lxappearance htop feh bash-completion reflector wireguard-tools
 #sudo pacman -S lightdm lightdm-gtk-greeter
 #sudo pacman -S i3 polybar
-#sudo pacman -S firefox 
-#sudo pacman -S wireguard-tools qbittorrent
+#sudo pacman -S firefox qbittorrent
