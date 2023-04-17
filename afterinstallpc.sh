7c032db414b94b23c6b253241ce5c7eb950b88d5
2nd1rst <2nd1rst@gmail.com>
2023-04-16 20:54:04 -0400

    2

--- afterinstallpc.sh
+++ afterinstallpc.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 sudo pacman -Syu
 sudo pacman -S firefox xclip ranger pcmanfm p7zip openssh neofetch mpv man-pages man-db lxappearance htop feh bash-completion reflector wireguard-tools qbittorrent 
-#sudo pacman -S lightdm lightdm-gtk-greeter
-#sudo pacman -S i3 polybar
 cd ~/repos
 git clone https://github.com/remisthb/main
 git clone https://github.com/remisthb/dwm
 git clone https://github.com/remisthb/dmenu
 git clone https://github.com/remisthb/st
 cd ~/repos/dwm
-sudo make clean install
+sudo make clean
+sudo make install
 cd ~/repos/dmenu
-sudo make clean install
+sudo make clean
+sudo make install
 cd ~/repos/st
-sudo make clean install
+sudo make clean
+sudo make install
 rm ~/.bashrc
 cp ~/repos/main/.bashrc ~/
 cp ~/repos/main/.vimrc ~/
